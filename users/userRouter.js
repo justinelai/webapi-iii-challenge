8a0a04a91b8aace996968e158786569b3548b3de
justinelai <justine.lai@gmail.com>
2019-09-04 16:24:11 -0400

    refactored all user CRUD with validate ID middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -23,6 +23,8 @@ router.post('/', (req, res) => {
 }
 });
 
+// ========================= POST /users/:id/posts =========================
+
 router.post('/:id/posts', (req, res) => {
     const {text} = req.body;
     const newComment = {...req.body, "user_id": req.params.id }
@@ -30,15 +32,16 @@ router.post('/:id/posts', (req, res) => {
         res.status(400).json({ errorMessage: "Please provide text for the post." })
     } else {
         Users.insertComment(newComment)
-            .then(inserted => 
+           /*  .then(inserted => 
                 {
                     Posts.findCommentById(inserted.id).then(fullComment => res.status(201).json((fullComment)))
                 })
-            .catch(err => res.status(500).json({ error: "There was an error while saving the post to the database" }))
+            .catch(err => res.status(500).json({ error: "There was an error while saving the post to the database" })) */
     }
 });
 
 // ========================= GET /users ========================= working!
+
 router.get('/', (req, res) => {
     Users.get()
     .then(result => {
@@ -49,24 +52,23 @@ router.get('/', (req, res) => {
     })
 })
 
-// ========================= GET /users/:id ========================= working!
-router.get('/:id', (req, res) => {
-    const id = req.params.id
-    Users.getById(id)
-    .then(u => {
-        u ? res.status(200).json(u) : res.status(404).json({ message: "The user with the specified ID does not exist." })
-    })
-    .catch(err => {
-        res.status(500).json({ error: "The user information could not be retrieved." })
+// ========================= GET /users/:id ========================= working! REFACTORED!
+router.get('/:id', validateUserId, (req, res) => {
+    Users.getById(req.params.id)
+        .then(u => {
+            res.status(200).json(u)
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The user information could not be retrieved." })
     })
 })
 
 // ========================= GET /users/:id/posts ========================= working!
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
     Users.getUserPosts(req.params.id)
     .then(result => {
-        result.length ? res.status(200).json(result) : res.status(404).json({ message: "The user with the specified ID does not exist." })
+        res.status(200).json(result)
     })
     .catch(err => {
         res.status(500).json({ error: "The user post information could not be retrieved" })
@@ -75,30 +77,34 @@ router.get('/:id/posts', (req, res) => {
 
 // ========================= DELETE /users/:id ========================= working!
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
     const id = req.params.id
     Users.remove(id)
     .then(user => {
-        user ? res.status(200).json(user) : res.status(404).json({ message: "The user with the specified ID does not exist." })
+        res.status(200).json(user)
     })
     .catch(err => {
         res.status(500).json({ error: "The user could not be removed" })
     })
 });
 
-router.put('/:id', (req, res) => {
+// ========================= PUT /users/:id ========================= 
+
+router.put('/:id', validateUserId, (req, res) => {
     const {name} = req.body;
+    const {id} = req.params;
     if (!name) {
         res.status(400).json({ errorMessage: "Please provide name for the user." })
     } else {
-        Users.update(req.params.id, req.body)
+        Users.update(id, req.body)
     .then(result => {
-        result.length ?
-        Users.getById(id).then(u => res.status(201).json(u)) :
-        res.status(404).json({ message: "The user with the specified ID does not exist." })
+            Users.getById(id)
+            .then(u => res.status(200).json(u))
+            .catch(err => res.status(500).json({ error: "There was an error while saving the user to the database" }))
     })
     .catch(err => {
-        res.status(500).json({ error: "There was an error while updating the user to the database" })
+        err.code ===  "SQLITE_CONSTRAINT" ? res.status(400).json({ errorMessage: "Name must be unique." })
+        : res.status(500).json({ error: "There was an error while saving the user to the database" })
     })
     }
     
@@ -107,7 +113,13 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-
+    Users.getById(req.params.id)
+        .then(u => {
+            u ? next() : res.status(404).json({ message: "The user with the specified ID does not exist." })
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The user information could not be retrieved." })
+    })
 };
 
 function validateUser(req, res, next) {
