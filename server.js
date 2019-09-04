64b9e06c64beabe94a7208e22205c19ab3d3efe0
justinelai <justine.lai@gmail.com>
2019-09-04 15:19:18 -0400

    create first logger middleware. working

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = 'express';
-
+const express = require('express');
 const server = express();
+server.use(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send("Hello world!")
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+    
+  );
+  next();
 };
 
 module.exports = server;
